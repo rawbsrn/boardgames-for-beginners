9d8c5d857d86acf068e8b06791d678603494c1cf
Robert (Rob) Sameh <robsameh@gmail.com>
2022-09-27 14:36:42 -0700

    formatted results page

--- website/src/features/games/Result.js
+++ website/src/features/games/Result.js
@@ -7,7 +7,7 @@ export const Result = () => {
 
   return (
     <>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <pre>With {JSON.stringify(state.game.players, null, 2)} players over {JSON.stringify(state.game.time, null, 2)} time, a category of {JSON.stringify(state.game.category, null, 2)} and with {JSON.stringify(state.game.mechanics, null, 2)}, I reccomend: </pre>
       <Link to="/">Start over</Link>
     </>
   )
